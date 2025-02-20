abf9283b3781bc688ccf5d7a95b097df7f4357c5
Stevesuk <stevesukawa@outlook.com>
2025-02-20 21:01:39 +0800

    像日吴旭醇

--- libs/olog.py
+++ libs/olog.py
@@ -1,5 +1,6 @@
 import datetime
 import sys
+import inspect
 
 from colorama import Fore, Style, Back
 
@@ -12,6 +13,6 @@ DEBUG = f'{Back.MAGENTA} DEBG {Back.RESET}'
 def output(*values: object, end: str = "\n", type: str = INFO):
     now = datetime.datetime.now()
     for i in values:
-        sys.stdout.write(f'{Back.GREEN} {now.strftime("%H:%M:%S")} {type} {i} {Style.RESET_ALL}')
+        sys.stdout.write(f'{Back.GREEN} {now.strftime("%H:%M:%S")} {Back.CYAN} {inspect.stack()[1].filename.replace('\\', '/').split('/')[-1][:-3]} {type} {i} {Style.RESET_ALL}')
 
     sys.stdout.write(f'{end}')
