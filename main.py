1bf3585d43abfdfb86eead860c0e9dd56fc5c18e
Stevesuk <stevesukawa@outlook.com>
2025-02-23 00:35:57 +0800

    version update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from libs.olog import output as log
 from libs.olog import WARN, ERROR, INFO, DEBUG
 
 gui._VERSION = 'dev'
-gui._SUBVERSION = '25w08f'
+gui._SUBVERSION = '25w09a'
 
 log(f'Starting ATNetwork ArkLaucher, version {gui._VERSION}-{gui._SUBVERSION}.')
 
