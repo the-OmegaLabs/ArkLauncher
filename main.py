30083ea6ec84fefb6c4c9b7138d7d1bb69b3818f
BIZONUSERYAKAMO <2860483411@qq.com>
2025-02-22 20:46:55 +0800

    refactor: RENAME

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ from libs.olog import WARN, ERROR, INFO, DEBUG
 gui._VERSION = 'dev'
 gui._SUBVERSION = '25w08f'
 
-log(f'Starting ATCraft ArkLaucher, version {gui._VERSION}-{gui._SUBVERSION}.')
+log(f'Starting ATNetwork ArkLaucher, version {gui._VERSION}-{gui._SUBVERSION}.')
 gui.main()
