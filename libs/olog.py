c45ab75b2436a8c1bcbea90dbba6fb6ae8b5e42e
Stevesuk <stevesukawa@outlook.com>
2025-03-02 10:35:09 +0800

    update license

--- libs/olog.py
+++ libs/olog.py
@@ -1,3 +1,18 @@
+# Copyright 2025 Omega Labs, ArkLauncher Contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import datetime
 import inspect
 import sys
@@ -20,19 +35,19 @@ logLevel = 5
 
 def output(value: str, end: str = "\n", type: str = Type.INFO):
     now = datetime.datetime.now()
-    if type == Type.DEBUG and logLevel == 5:
+    if type == Type.DEBUG:
         sys.stdout.write(f"{Back.GREEN} {now.strftime('%H:%M:%S')} {Back.CYAN} " +
                          inspect.stack()[1].filename.replace('\\', '/').split('/')[-1][
                          :-3] + f" {type} {value} {Style.RESET_ALL}")
-    elif type == Type.ERROR and logLevel >= 4:
+    elif type == Type.ERROR:
         sys.stdout.write(f"{Back.GREEN} {now.strftime('%H:%M:%S')} {Back.CYAN} " +
                          inspect.stack()[1].filename.replace('\\', '/').split('/')[-1][
                          :-3] + f" {type} {value} {Style.RESET_ALL}")
-    elif type == Type.WARN and logLevel >= 3:
+    elif type == Type.WARN:
         sys.stdout.write(f"{Back.GREEN} {now.strftime('%H:%M:%S')} {Back.CYAN} " +
                          inspect.stack()[1].filename.replace('\\', '/').split('/')[-1][
                          :-3] + f" {type} {value} {Style.RESET_ALL}")
-    elif type == Type.INFO and logLevel >= 1:
+    elif type == Type.INFO:
         sys.stdout.write(f"{Back.GREEN} {now.strftime('%H:%M:%S')} {Back.CYAN} " +
                          inspect.stack()[1].filename.replace('\\', '/').split('/')[-1][
                          :-3] + f" {type} {value} {Style.RESET_ALL}")
