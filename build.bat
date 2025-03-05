ee21a01a39e899bf47bae363fc43cfe93ad085b4
Stevesuk <stevesukawa@outlook.com>
2025-03-05 23:02:23 +0800

    build with upx

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-pyinstaller --onefile --optimize 2 --hide-console minimize-early -i ark.ico --uac-admin ark.py
+pyinstaller --onefile --optimize 2 --upx-dir ./upx --hide-console minimize-early -i ark.ico --uac-admin ark.py
