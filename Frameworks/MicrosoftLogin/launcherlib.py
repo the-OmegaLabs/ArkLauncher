18704e6cc01ba44cb743255b5534ba098a88ae36
BIZONUSERYAKAMO <2860483411@qq.com>
2025-04-28 21:49:00 +0800

    9

--- Frameworks/MicrosoftLogin/launcherlib.py
+++ Frameworks/MicrosoftLogin/launcherlib.py
@@ -2,7 +2,9 @@ import urllib
 import urllib.parse
 
 import minecraft_launcher_lib
+import requests
 
+FALLBACK_PORT = 13372
 AUTH_URL="https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize"
 TOKEN_URL="https://login.microsoftonline.com/consumers/oauth2/v2.0/token"
 SCOPE="XboxLive.signin offline_access"
@@ -30,6 +32,6 @@ def getLogin(client_id: str, redirect_uri: str) -> str:
 #     client_id="76658556-e195-49da-a47e-3c1eb90f6f9b",
 #     redirect_uri="http://localhost:13372"
 # )
-loginurl, state, verifier = minecraft_launcher_lib.microsoft_account.get_secure_login_data(    client_id="76658556-e195-49da-a47e-3c1eb90f6f9b",
+loginurl, state, verifier = minecraft_launcher_lib.microsoft_account.get_secure_login_data(    client_id="ece1bc0c-e3d1-4967-b4a2-63d13c57380c",
     redirect_uri="http://localhost:13372")
 print(loginurl)
