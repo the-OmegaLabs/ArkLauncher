0c945ac4be67330a79f3f01e036eef4c54dbd540
Stevesuk <stevesukawa@outlook.com>
2025-04-29 23:05:32 +0900

    a set of sound effect

--- Frameworks/Notify.py
+++ Frameworks/Notify.py
@@ -1,7 +1,9 @@
 import maliang
 import maliang.animation
+import threading
+from playsound import playsound
 
-ANIMATION_DURATION = 700
+ANIMATION_DURATION = 300
 ANIMATION_FPS = 1000
 SCALE = 1
 FONT_FAMILY      = f'Microsoft YaHei UI'
@@ -11,25 +13,28 @@ HEIGHT = 100
 
 def scaled(n): return n * SCALE
 
-def toast(title, message, duration, icon, root):
-    toast = maliang.Toplevel(root, size=(scaled(WIDTH), scaled(HEIGHT)), position=(root.winfo_screenwidth(), scaled(50)), focus=False)
+def toast(title, message, duration, icon, root, target = 'notify'):
+    def play():
+        threading.Thread(target=playsound, args=[f'Resources/sounds/{target}.mp3'], daemon=True).start()
+
+    play()
+    toast = maliang.Toplevel(root, size=(scaled(WIDTH), scaled(HEIGHT)), position=(10000, 10000), focus=False)
+    toast.alpha(0.85)
+    toast.geometry(position=(toast.winfo_screenwidth() // 2 - scaled(WIDTH) // 2, 0 - scaled(HEIGHT)))
     toast.overrideredirect(True)
     cv = maliang.Canvas(toast)
     cv.place(x = 0, y = 0, width = scaled(WIDTH), height=scaled(HEIGHT))
 
     maliang.animation.MoveWindow(
-        window = toast, offset = (0 - scaled(WIDTH), 0), 
+        window = toast, offset = (0, scaled(HEIGHT)), 
         duration = ANIMATION_DURATION, 
         controller = maliang.animation.ease_out, fps = ANIMATION_FPS
     ).start()
     
     maliang.animation.MoveWindow(
-        window = toast, 
-        offset = (scaled(WIDTH), 0), 
-        duration = ANIMATION_DURATION + 200, 
-        controller = maliang.animation.ease_out, 
-        fps = ANIMATION_FPS, 
-        end = toast.destroy
+        window = toast, offset = (0, 0 - scaled(HEIGHT)), 
+        duration = int(ANIMATION_DURATION * 1.5), fps = ANIMATION_FPS, 
+        #end = lambda: (cv.after(1000, toast.destroy))
     ).start(delay=duration - ANIMATION_DURATION - 200)
     
     maliang.Image(
@@ -45,4 +50,6 @@ def toast(title, message, duration, icon, root):
     maliang.Text(
         master = cv, position = (scaled(HEIGHT // 1.1), scaled(HEIGHT // 1.75)),
         text=message, family=FONT_FAMILY, fontsize=16
-)
+
+    )
+
--- ark.py
+++ ark.py
@@ -513,7 +513,7 @@ def mainPage(*_):
     settings            = maliang.IconButton(bottomSettingsMask, (0, 0), (bottomMaskHEIGHT - 4, bottomMaskHEIGHT - 4), image=maliang.PhotoImage(getImage('icon_settings').resize((40, 40), 1)), command=lambda: changeWindow(settingsPage, extra_args=(4)), anchor='center')
     account             = maliang.IconButton(bottomAccountMask, (0, 0), (bottomMaskHEIGHT - 4, bottomMaskHEIGHT - 4), image=maliang.PhotoImage(getImage('icon_account').resize((40, 40), 1)), command=lambda: changeWindow(settingsPage, extra_args=(0)), anchor='center')
 
-    launch              = maliang.Button(bottomLaunchMask, (0, 0), size=(480, 116), command=lambda: Notify.toast(title='testTitle', message='testMessage', duration=5000, root=root, icon=getImage('icon_logo').resize((60, 60), 1)))
+    launch              = maliang.Button(bottomLaunchMask, (0, 0), size=(480, 116), command=lambda: Notify.toast(title='testTitle', message='testMessage', duration=2000, root=root, icon=getImage('icon_logo').resize((60, 60), 1)))
     launchIcon          = maliang.Image(launch, (bottomLMaskHEIGHT // 2 - 5, bottomLMaskHEIGHT // 2 - 7),image=maliang.PhotoImage(getImage('icon_launch').resize((80, 80), 1)), anchor='center')
     launchDesc          = maliang.Text(launch, position=(105, bottomLMaskHEIGHT // 2 - 35), text='Launch game', family=FONT_FAMILY,fontsize=17)
     launchTitle         = maliang.Text(launch, position=(105, bottomLMaskHEIGHT // 2 - 10), text='Meira Client', family=FONT_FAMILY_BOLD, fontsize=25)
